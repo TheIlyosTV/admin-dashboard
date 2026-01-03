eef2b777451e5d20348340fa042e25d255a78da0
Axmedjanov Ilyosbek <theilyostv@gmail.com>
2026-01-03 07:36:04 +0500

    Layout fix commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,10 +5,39 @@ import { AuthProvider } from '@/contexts/AuthContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
+
 export const metadata: Metadata = {
-  title: 'Admin Dashboard',
-  description: 'Modern Admin Dashboard',
+  title: "Admin Dashboard",
+  description: "Modern Admin Dashboard",
+  authors: [{ name: "Frontend Axmedjanov" }],
+  openGraph: {
+    title: "Admin Dashboard",
+    description: "Modern Admin Dashboard",
+    url: "https://admin-dashboard.vercel.app",
+    siteName: "admin-dashboard",
+    images: [
+      {
+        url: "/og-imagee.jpg", 
+        width: 1200,
+        height: 630,
+        alt: "admin-dashboard — modern admin panel",
+      },
+    ],
+    locale: "uz_UZ",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Admin Dashboard",
+    description: "Modern Admin Dashboard",
+    images: ["/og-imagee.jpg"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
 };
+
 
 export default function RootLayout({
   children,
